7ea7a08f50745aed41d71cfba11a0e84ca157a6c
Khaled <achechkhaled@gmail.com>
2020-10-13 17:48:25 +0200

    adding demo version

---
+++ src/main/java/com/TekUp/VentTNDemo/VentTnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.TekUp.VentTNDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentTnDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentTnDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TekUp/VentTNDemo/VentTnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TekUp.VentTNDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentTnDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
