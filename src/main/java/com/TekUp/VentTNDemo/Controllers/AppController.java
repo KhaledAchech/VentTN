5ecf8e66678a50b5ccceb7b6027c0a849ba59f34
Khaled <achechkhaled@gmail.com>
2021-01-06 23:27:38 +0100

    admin dashboard

--- src/main/java/com/TekUp/VentTNDemo/Controllers/AppController.java
+++ src/main/java/com/TekUp/VentTNDemo/Controllers/AppController.java
@@ -1,12 +1,15 @@
 package com.TekUp.VentTNDemo.Controllers;
 
 import com.TekUp.VentTNDemo.Model.User;
+import com.TekUp.VentTNDemo.Repositories.ProductRepo;
 import com.TekUp.VentTNDemo.Repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /************************************
  ********* author : Khaled ***********
@@ -18,10 +21,12 @@ public class AppController {
     @Autowired
     private UserRepo userRepo;
     private UserController userController;
+    private final ProductRepo productrepo;
 
-    public AppController(UserRepo userRepo, UserController userController) {
+    public AppController(UserRepo userRepo, UserController userController, ProductRepo productrepo) {
         this.userRepo = userRepo;
         this.userController = userController;
+        this.productrepo = productrepo;
     }
 
     /******************************** Authentication ************************************************/
@@ -42,4 +47,16 @@ public class AppController {
         return "index";
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping("/dashboard")
+    public String adminDashboard()
+    {
+        return "Admin/dashboard";
+    }
+    @RequestMapping("/products")
+    public void getProducts(Model model)
+    {
+        model.addAttribute("products", productrepo.findAll());
+
+    }
 }
